d6b624511e8c36c1c323c91d2013dba7933f9f36
724553291@qq.com <724553291@qq.com>
2018-09-17 17:10:43 +0800

    方法重写

--- src/DingtalkEapp.php
+++ src/DingtalkEapp.php
@@ -3,18 +3,26 @@
 namespace Ghlin\DingtalkEapp;
 
 use Ghlin\DingtalkEapp\Util\Http;
+use Ghlin\DingtalkEapp\Crypto\DingtalkCrypt;
 
 class DingtalkEapp {
     
+    private $suiteKey;
+    private $suiteSecret;
+
+    public function __construct() {
+        $this->suiteKey = config('dingtalkeapp.suite_key');
+        $this->suiteSecret = config('dingtalkeapp.suite_secret');
+    }
+    
     public function getCorpToken($authCorpId) {
-        $suiteKey = config('dingtalkeapp.suite_key');
         $timeStamp = time() * 1000;
-        $suiteTicket = $this->getSuiteTicket($suiteKey);
+        $suiteTicket = $this->getSuiteTicket($this->suiteKey);
         $msg = $timeStamp."\n".$suiteTicket;
-        $sha = urlencode(base64_encode(hash_hmac('sha256', $msg, $suiteKey, true)));
+        $sha = urlencode(base64_encode(hash_hmac('sha256', $msg, $this->suiteSecret, true)));
         $res = Http::post("/service/get_corp_token",
             array(
-                "accessKey" => $suiteKey,
+                "accessKey" => $this->suiteKey,
                 "timestamp" => $timeStamp,
                 "suiteTicket" => $suiteTicket,
                 "signature" => $sha,
@@ -24,16 +32,61 @@ class DingtalkEapp {
         return $res;
     }
     
+    public function getAuthCorpInfo($authCorpId) {
+        $timeStamp = time() * 1000;
+        $suiteTicket = $this->getSuiteTicket($this->suiteKey);
+        $msg = $timeStamp."\n".$suiteTicket;
+        $sha = urlencode(base64_encode(hash_hmac('sha256', $msg, $this->suiteSecret, true)));
+        $res = Http::post("/service/get_auth_info",
+            array(
+                "accessKey" => $this->suiteKey,
+                "timestamp" => $timeStamp,
+                "suiteTicket" => $suiteTicket,
+                "signature" => $sha,
+            ),json_encode(array(
+                "auth_corpid" => $authCorpId,
+            )));
+        return $res->auth_corp_info;
+    }
+    
     public function getUserInfo($accessToken, $code) {
+        
         $res = Http::get("/user/getuserinfo",
             array(
                 "access_token" => $accessToken,
                 "code" => $code,
             ));
-        return $res;
+        $detail = Http::get("/user/get",
+            array(
+                "access_token" => $accessToken,
+                "userid" => $res->userid,
+            ));
+        return $detail;
     }
     
     public function getSuiteTicket($suiteKey) {
         return 'temp_suite_ticket_only4_test';
     }
+    
+    public function DecryptMsg($signature, $timeStamp, $nonce, $postdata, $decryptMsg) {
+        $token = config('dingtalkeapp.dingdong');
+        $encodingAesKey = config('dingtalkeapp.encoding_aes_key');
+        $suiteKey = config('dingtalkeapp.suite_key');
+        $postList = json_decode($postdata,true);
+        $encrypt = $postList['encrypt'];
+
+        $crypt = new DingtalkCrypt($token, $encodingAesKey, $suiteKey);
+        $errCode = $crypt->DecryptMsg($signature, $timeStamp, $nonce, $encrypt, $decryptMsg);
+        return $errCode;
+    }
+    
+    public function EncryptMsg($res, $timeStamp, $nonce, $encryptMsg) {
+        $token = config('dingtalkeapp.dingdong');
+        $encodingAesKey = config('dingtalkeapp.encoding_aes_key');
+        $suiteKey = config('dingtalkeapp.suite_key');
+        $crypt = new DingtalkCrypt($token, $encodingAesKey, $suiteKey);
+        $errCode = $crypt->EncryptMsg($res, $timeStamp, $nonce, $encryptMsg);
+        return $errCode;
+    }
+    
 }
